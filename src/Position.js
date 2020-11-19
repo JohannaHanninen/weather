659041e545512f007b0fbb2975195b2795268ef3
JohannaHanninen <c0hajo02@students.oamk.fi>
2020-11-19 15:18:46 +0200

    location ei toimi

---
+++ src/Position.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import Weather from './Weather';
+
+export default class Position extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lat: 0,
+            lng: 0
+        }
+    }
+
+    componentDidMount() {
+        if (navigator.geoLocation) {
+            navigator.geoLocation.getCurrentPosition(position => {
+                this.setState({
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                });
+            }, (error) => {
+                alert(error);
+            })
+        } else {
+            alert("Your browser does not support geolocation");
+        }
+    }
+
+
+    render() {
+        const {lat, lng, isLoaded} = this.state;
+        if (isLoaded) {
+           return (
+            <>
+                <h3>Your position is</h3>
+                <p>Position: {lat.toFixed(3)}, {lng.toFixed(3)}</p> 
+                <Weather lat={lat} lng={lng} />
+            </>
+            ) 
+        } else {
+            return (<p>Loading...</p>)
+        }
+    }
+}
